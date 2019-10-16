f1086236232db418e2f2a977781fbc2b9ea46756
Mathieu <mathieu.gery@epitech.eu>
2019-10-16 17:02:48 +0200

    [+] NavBar with search and home and categories

---
+++ lib/homePage.dart
@@ -0,0 +1,166 @@
+import './ImgurImage.dart';
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<StatefulWidget> createState() {
+    return _MyHomePageState();
+  }
+  // _MyHomePageState createState() => _MyHomePageState();
+
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  var mPageCount = 0;
+  bool isLoading = false;
+  int itemType = ImgurImage.TYPE_PROGRESS;
+  List<ImgurImage> imageList = [];
+  ScrollController _controller;
+
+  _scrollListener() {
+    if (_controller.offset >= _controller.position.maxScrollExtent &&
+        !_controller.position.outOfRange) {
+      _fetchImages();
+    }
+  }
+
+  @override
+  void initState() {
+    _controller = ScrollController();
+    _controller.addListener(_scrollListener);
+    super.initState();
+    _fetchImages();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: _homePage(),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            DrawerHeader(
+              child: Text('Drawer Header'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _homePage() {
+    if (imageList.length == 0 ||
+        (imageList.length == 1 &&
+            imageList[0].itemType == ImgurImage.TYPE_PROGRESS)) {
+      return Center(child: CircularProgressIndicator());
+    } else if (imageList.length == 1 &&
+        imageList[0].itemType == ImgurImage.TYPE_ERROR) {
+      return Center(
+        child: RaisedButton(
+          onPressed: () {
+            _fetchImages();
+          },
+          child: Text('Try Again'),
+        ),
+      );
+    } else {
+      itemType = imageList[imageList.length - 1].itemType;
+      return Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.all(10),
+              //crossAxisCount: 1,
+              controller: _controller,
+              children: List.generate(
+                imageList.length,
+                (index) {
+                  var image = imageList[index];
+                  if (image.itemType == ImgurImage.TYPE_ITEM) {
+                    return Center(
+                      widthFactor: 10,
+                      child: FadeInImage.assetNetwork(
+                          placeholder: 'assets/load.jpeg', image: image.link),
+                    );
+                  } else {
+                    return Container();
+                  }
+                },
+              ),
+            ),
+          ),
+          _showIndicator(),
+        ],
+      );
+    }
+  }
+
+  Widget _showIndicator() {
+    if (itemType == ImgurImage.TYPE_PROGRESS) {
+      return Container(
+        margin: EdgeInsets.all(20),
+        child: Center(child: CircularProgressIndicator()),
+      );
+    } else {
+      return Container();
+    }
+  }
+
+  void _fetchImages() async {
+    if (!isLoading) {
+      mPageCount++;
+      isLoading = true;
+
+      if (imageList.length == 1) imageList.removeLast();
+      imageList.add(ImgurImage(link: "", itemType: ImgurImage.TYPE_PROGRESS));
+      //setState(() {});
+
+      await fetchImages(mPageCount).then((imgurImages) {
+        imageList.removeLast();
+        for (var value in imgurImages.images) {
+          if (value != null) {
+            imageList.add(value);
+          }
+        }
+      }).catchError((error) {
+        imageList.removeLast();
+        if (imageList.length == 0)
+          imageList.add(ImgurImage(link: "", itemType: ImgurImage.TYPE_ERROR));
+        if (mPageCount > 0) {
+          mPageCount--;
+        }
+      }).whenComplete(() {
+        isLoading = false;
+        setState(() {});
+      });
+    }
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,144 +1,44 @@
-import 'package:flutter_app/ImgurImage.dart';
+import 'package:flutter_app/navbar.dart';
+
+import './homePage.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primaryColor: Colors.black,
+        primaryColor: Colors.blue,
       ),
-      home: MyHomePage(title: 'Random Pics'),
+      home: NavBar(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var mPageCount = 0;
-  bool isLoading = false;
-  int itemType = ImgurImage.TYPE_PROGRESS;
-  List<ImgurImage> imageList = [];
-  ScrollController _controller;
-
-  _scrollListener() {
-    if (_controller.offset >= _controller.position.maxScrollExtent &&
-        !_controller.position.outOfRange) {
-      _fetchImages();
-    }
-  }
-
-  @override
-  void initState() {
-    _controller = ScrollController();
-    _controller.addListener(_scrollListener);
-    super.initState();
-    _fetchImages();
-  }
-
+/*
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _loadView(),
-    );
-  }
-
-  Widget _loadView() {
-    if (imageList.length == 0 ||
-        (imageList.length == 1 &&
-            imageList[0].itemType == ImgurImage.TYPE_PROGRESS)) {
-      return Center(child: CircularProgressIndicator());
-    } else if (imageList.length == 1 &&
-        imageList[0].itemType == ImgurImage.TYPE_ERROR) {
-      return Center(
-        child: RaisedButton(
-          onPressed: () {
-            _fetchImages();
-          },
-          child: Text('Try Again'),
+    return MaterialApp(
+        theme: ThemeData(
+          primaryColor: Colors.blue,
         ),
-      );
-    } else {
-      itemType = imageList[imageList.length - 1].itemType;
-      return Column(
-        children: <Widget>[
-          Expanded(
-            child: GridView.count(
-              crossAxisCount: 3,
-              controller: _controller,
-              children: List.generate(
-                imageList.length,
-                    (index) {
-                  var image = imageList[index];
-                  if (image.itemType == ImgurImage.TYPE_ITEM) {
-                    return Center(
-                        child: FadeInImage.assetNetwork(
-                            placeholder: 'assets/load.jpeg',
-                            image: image.link)
-                    );
-                  } else {
-                    return Container();
-                  }
-                },
-              ),
-            ),
+        //home: MyHomePage(title: 'Random Pics'),
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('bite'),
           ),
-          _showIndicator(),
-        ],
-      );
-    }
-  }
-
-  Widget _showIndicator() {
-    if (itemType == ImgurImage.TYPE_PROGRESS) {
-      return Container(
-        margin: EdgeInsets.all(20),
-        child: Center(child: CircularProgressIndicator()),
-      );
-    } else {
-      return Container();
-    }
-  }
-
-  void _fetchImages() async {
-    if (!isLoading) {
-      mPageCount++;
-      isLoading = true;
-
-      if (imageList.length == 1) imageList.removeLast();
-      imageList.add(ImgurImage(link: "", itemType: ImgurImage.TYPE_PROGRESS));
-      setState(() {});
-
-      await fetchImages(mPageCount).then((imgurImages) {
-        imageList.removeLast();
-        for (var value in imgurImages.images) {
-          if (value != null) {
-            imageList.add(value);
-          }
-        }
-      }).catchError((error) {
-        imageList.removeLast();
-        if (imageList.length == 0)
-          imageList.add(ImgurImage(link: "", itemType: ImgurImage.TYPE_ERROR));
-        if (mPageCount > 0) {
-          mPageCount--;
-        }
-      }).whenComplete(() {
-        isLoading = false;
-        setState(() {});
-      });
-    }
+        ));
   }
 }
+*/
